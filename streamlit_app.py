3fe6bae6569e8e6ee7731100fe32c7cd9b895661
DbaMountaineer <33457054+DbaMountaineer@users.noreply.github.com>
2023-09-28 04:01:11 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ streamlit.dataframe(fruityvice_normalized)
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-#my_cur.execute("select * from fruit_load_list")
+
 #This will not work correctly, but just go with it for now
 my_cur.execute("insert into fruit_load_lists values ('from streamlit')")
 my_data_rows = my_cur.fetchall()
