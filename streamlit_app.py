b37a0c034440d5eca8eb1190cd0a83eb22e6c5d6
DbaMountaineer <33457054+DbaMountaineer@users.noreply.github.com>
2023-09-27 22:14:10 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,5 +23,5 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display furityvice api response
 import requests
-furityvice_response =  requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response =  requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
